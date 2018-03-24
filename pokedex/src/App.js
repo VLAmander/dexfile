49ce4dda7884897307bc0ad91dd061517a72f161
VLAmander <clouddavis0296@yahoo.com>
2018-03-24 16:06:21 -0500

    line one

---
+++ pokedex/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          <h1 className="App-title">poke dex</h1>
+        </header>
+    <body>
+      <div left>
+      
+        <div left>
+      </div>
+      <div right>
+
+      </div>
+    </body>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
